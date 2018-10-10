466a8c1d0f2ceef93c4747f1ba88da082ee23c19
Tuqui Braun <tuquibraun@gmail.com>
2018-10-10 14:47:01 -0300

    nav work

--- js/main.js
+++ js/main.js
@@ -6,10 +6,16 @@ $(function(){
         $('.nav').removeClass('content-hidden')
     }, 500);
 
-    $('.icon').click(function() {
-        $('.icon').toggleClass('active');
-    })
+
+    var navEl = $('.nav'),
+        navToggleBtn = navEl.find('.icon.toggle-btn');
     
+    navToggleBtn.on('click' , function(e) {
+        navEl.toggleClass('active');
+        navToggleBtn.toggleClass('active');
+
+    })
+
     window.addEventListener('scroll', function(){
         var currScrollPos = $(document).scrollTop();
         if (currScrollPos > 300) {
