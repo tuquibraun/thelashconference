702eff92b9152622f6f1edb5d131175335f201bb
santiagocollomb <santiagocollomb@gmail.com>
2018-10-17 20:04:26 -0300

    modelo v c

---
+++ controladores/plantilla.controlador.php
@@ -0,0 +1,15 @@
+<?php
+
+class ControladorPlantilla{
+
+	/*=============================================
+	LLAMAMOS LA PLANTILLA
+	=============================================*/
+
+	public function plantilla(){
+
+		include "vistas/plantilla.php";
+
+	}
+
+}
---
+++ vistas/js/main.js
@@ -0,0 +1,38 @@
+$(function(){
+    var welcomeSection = $('.welcome-section');
+
+    setTimeout(function(){
+        welcomeSection.removeClass('content-hidden'),
+        $('.nav').removeClass('content-hidden')
+    }, 500);
+
+
+    var navEl = $('.nav'),
+        navToggleBtn = navEl.find('.icon.toggle-btn');
+    
+    navToggleBtn.on('click' , function(e) {
+        navEl.toggleClass('active');
+        navToggleBtn.toggleClass('active');
+
+    })
+
+    window.addEventListener('scroll', function(){
+        var currScrollPos = $(document).scrollTop();
+        var people = $('.people-section').offset();
+        people = people.top - 450;
+        var sponsor = $('.sponsor-section').offset();
+        sponsor  = sponsor   .top - 500;
+        if (currScrollPos > people) {
+            $('.people-section').removeClass('content-hidden');
+        }else{
+            $('.people-section').addClass('content-hidden');
+        }
+        if (currScrollPos > sponsor) {
+            $('.sponsor-section').removeClass('content-hidden');
+        }else{
+            $('.sponsor-section').addClass('content-hidden');
+        }
+    })
+
+    
+})
